0341d942b9e04005a54deb0d8832191653c6c77b
Abdurrahman Kaan Dikeç <dikeckaan@gmail.com>
2026-09-16 09:49:07 +0300

    Add easy-vphone 0.2.0 cask

---
+++ Casks/easy-vphone.rb
@@ -0,0 +1,16 @@
+cask "easy-vphone" do
+  version "0.2.0"
+  sha256 "3e8eaa28c914b6333070f688e272c4d780577346537f24a2e0861c721a9251b3"
+
+  url "https://github.com/dikeckaan/easy-vphone/releases/download/v#{version}/easy-vphone-#{version}-arm64.zip"
+  name "easy-vphone"
+  desc "Graphical iOS virtual machine toolkit with an integrated IPA library"
+  homepage "https://github.com/dikeckaan/easy-vphone"
+
+  depends_on arch: :arm64
+  depends_on macos: ">= :sequoia"
+
+  app "easy-vphone.app"
+
+  zap trash: "~/Library/Preferences/io.github.easy-vphone.plist"
+end
---
+++ README.md
@@ -0,0 +1,10 @@
+# Homebrew tap for easy-vphone
+
+```sh
+brew tap dikeckaan/easy-vphone
+brew install --cask dikeckaan/easy-vphone/easy-vphone
+```
+
+Apple Silicon, macOS 15 or later. Version 0.2.0 is an Apple Development signed development preview, not a notarized Developer ID release. Gatekeeper may block the app on other Macs.
+
+Source and release notes: https://github.com/dikeckaan/easy-vphone
